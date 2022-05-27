04beed328da9cf36d6cbaac542c86e33fce6a41c
khs3z <khs3z@virginia.edu>
2022-05-26 21:06:07 -0400

    SLURM job array

--- avi-creator.py
+++ avi-creator.py
@@ -1,7 +1,6 @@
 #@ File (label="Input directory", style="directory") inputdir 
 #@ File (label="Output directory", style="directory") outputdir 
 #@ String (label="Position") pos
-#@ Boolean (label="Register Stack", value=False) register 
 
 import sys
 import math
@@ -17,33 +16,30 @@ def get_file_list(inputdir, img_ext=["tif", "tiff"], pos=''):
     img_group = [os.path.join(inputdir,f) for f in files if pos in f and f.split(".")[-1].lower() in img_ext]
     return img_group
 
+def sort_func(item):
+    #example: scan_Top Slide_R_p99_0_A01f12d3.TIF
+    fname = os.path.basename(item)
+    t = int(fname.split('_')[3][1:])
+    return t
+
 def sort_files(file_list):
-    return sorted(file_list)
+    return sorted(file_list,key=sort_func)
 
 def open_image(imgfile):
     options = ImporterOptions()
     options.setId(imgfile)
     options.setSplitChannels(False)
-    # options.setColorMode(ImporterOptions.COLOR_MODE_COMPOSITE)
     imps = BF.openImagePlus(options)
     return imps[0]
 
-def create_stack(file_list, stack_name, register=True):
-    file_list = [file_list[i] for i in range(3)]
+def create_stack(file_list, stack_name):
     imps = [open_image(f) for f in file_list]
     #imp_array = array(imps, ImagePlus)
     stack = ImageStack(imps[0].getWidth(), imps[0].getHeight())
     for slice in imps:
         stack.addSlice(slice.getProcessor())
     stack_imp = ImagePlus(stack_name, stack)
-    if register: 
-        IJ.run(stack_imp, "Linear Stack Alignment with SIFT", "initial_gaussian_blur=1.60 steps_per_scale_octave=3 minimum_image_size=64 maximum_image_size=1024 feature_descriptor_size=4 feature_descriptor_orientation_bins=8 closest/next_closest_ratio=0.92 maximal_alignment_error=25 inlier_ratio=0.05 expected_transformation=Rigid interpolate");
-        registered = IJ.getImage()
-        return registered
-    else:
-        return stack_imp
-
-    
+    return stack_imp
 
 def create_filename(pos):
     fname = os.path.splitext(pos)[0]
@@ -57,16 +53,21 @@ def save_avi(image_stack,avi_file):
 # Main code
 inputdir = str(inputdir)
 outputdir = str(outputdir)
+print 'Searching for files in', inputdir
 if not os.path.isdir(inputdir):
     print inputdir, 'does not exist or is not a directory.'
 else:
+    print 'Processing', pos
     file_list = get_file_list(inputdir, pos=pos)
     if len(file_list) > 0:
         sorted_list = sort_files(file_list)
+        for fn in sorted_list:
+            print fn
         stack_name = create_filename(pos)
-        stack_imp = create_stack(sorted_list, stack_name, register)
+        stack_imp = create_stack(sorted_list, stack_name)
         avi_file = os.path.join(outputdir,stack_name)
         save_avi(stack_imp, avi_file)
+        print 'Saved', avi_file
     else:
-        print ('No images found that match', pos,'.')  
+        print 'No images found that match', pos,'.'  
 print 'Done.\n'
--- start-job.sh
+++ start-job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 INPUT_DIR=${1}
+OUTPUT_DIR=${2}
 JOB_INPUT=input.txt
 
 if ! [ -d "${INPUT_DIR}" ]; then
@@ -8,9 +9,13 @@ if ! [ -d "${INPUT_DIR}" ]; then
     exit 1
 fi
 
+# read tif files, extract unique position identifier, and save in input file
 for file in ${INPUT_DIR}/*.TIF ; 
     do basename "$file" | awk -F_ '{print$6}'; 
 done | sort | uniq > ${JOB_INPUT}
 
 NO_GROUPS=$( < ${JOB_INPUT} wc -l )
 echo "Found ${NO_GROUPS} image groups in ${JOB_INPUT}"
+
+# launch job array
+sbatch --array=1-${NO_GROUPS} create_avi.slurm ${INPUT_DIR} ${OUTPUT_DIR} ${JOB_INPUT}
